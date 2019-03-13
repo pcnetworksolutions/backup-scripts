8b84ed0f7c485e20f1e84c463ae1116407180619
pcnkenton <47666785+pcnkenton@users.noreply.github.com>
2019-03-13 15:07:42 -0400

    Create backup-sql-db.ps1

---
+++ backup-sql-db.ps1
@@ -0,0 +1,18 @@
+
+
+Import-Module SQLPS
+
+
+
+
+
+# Reference articles
+#
+# Back up full database in general, includes PowerShell snippet
+# https://docs.microsoft.com/en-us/previous-versions/sql/sql-server-2012/ms186289(v%3dsql.110)
+#
+# Backup-SqlDatabase help page
+# https://docs.microsoft.com/en-us/powershell/module/sqlserver/backup-sqldatabase?view=sqlserver-ps
+#
+# Includes PS snippet for purging old backups
+# https://blog.sqlauthority.com/2018/04/02/sql-server-powershell-script-delete-old-backup-files-in-sql-express/
